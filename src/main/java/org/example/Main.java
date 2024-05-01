dc35c4bb7f6a1a2600e3a0a0a4400bac5b23bca4
ArtomBusniuk <151449253+ArtomBusniuk@users.noreply.github.com>
2024-05-01 22:38:54 +0100

    fix after CR

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,16 @@
-import javax.swing.*;
+package org.example;
 import java.util.Scanner;
 
 public class Main {
+
+    public static final String CORRECT_NUMBER = "5";
+
     public static void main(String[] args) {
         Scanner number = new Scanner(System.in);
-        System.out.println("Wpisz 1 albo 2:");
-        String  name = number.nextLine();
+        System.out.println("Wpisz " + CORRECT_NUMBER + " lub inny numer");
+        String name = number.nextLine();
         System.out.println(name);
-        if ("1".equals (name)) {
+        if (CORRECT_NUMBER.equals(name)) {
             System.out.println("Wszedles");
         } else {
             System.out.println("Nie prawidlowe haslo");
