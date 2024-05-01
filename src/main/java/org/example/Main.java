cc38f431453b37e26c6e0858e0694888026e6c0b
ArtomBusniuk <151449253+ArtomBusniuk@users.noreply.github.com>
2024-05-01 18:30:37 +0100

    create an early program which input numbers from user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import lombok.Getter;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@Getter
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
