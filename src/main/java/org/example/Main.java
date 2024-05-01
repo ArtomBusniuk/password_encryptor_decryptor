67c32f4056e0995345b1dd194f7b99f13220a758
ArtomBusniuk <151449253+ArtomBusniuk@users.noreply.github.com>
2024-05-01 22:02:18 +0100

    create an early program which input numbers from user (#2)
    * create an early program which input numbers from user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,22 @@
 package org.example;
+import java.util.Scanner;
 
-import lombok.Getter;
+import static java.lang.System.in;
+import static java.lang.System.out;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-@Getter
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static final String CORRECT_NUMBER = "5";
+
+    public static void main(String[] args) {
+        Scanner number = new Scanner(in);
+        out.println("Wpisz " + CORRECT_NUMBER + " lub inny numer");
+        String name = number.nextLine();
+        out.println(name);
+        if (CORRECT_NUMBER.equals(name)) {
+            out.println("Wszedles");
+        } else {
+            out.println("Nie prawidlowe haslo");
         }
     }
 }
