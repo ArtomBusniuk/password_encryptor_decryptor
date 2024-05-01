48e6d5bf97872fb249e8e93363969143fd6d75d7
ArtomBusniuk <151449253+ArtomBusniuk@users.noreply.github.com>
2024-05-01 18:36:27 +0100

    create an early program which input numbers from user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,16 @@
-package org.example;
+import javax.swing.*;
+import java.util.Scanner;
 
-import lombok.Getter;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-@Getter
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner number = new Scanner(System.in);
+        System.out.println("Wpisz 1 albo 2:");
+        String  name = number.nextLine();
+        System.out.println(name);
+        if ("1".equals (name)) {
+            System.out.println("Wszedles");
+        } else {
+            System.out.println("Nie prawidlowe haslo");
         }
     }
 }
