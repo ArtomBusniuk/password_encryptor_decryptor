71cd3f8c46138ae4061f116ddaf10af5b09c99a5
ArtomBusniuk <151449253+ArtomBusniuk@users.noreply.github.com>
2024-05-05 19:19:25 +0100

    added loop to allow users to retry login for specified ammount of 5 times

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,31 @@
 package org.example;
 import java.util.Scanner;
 
-import static java.lang.System.in;
-import static java.lang.System.out;
+import static java.lang.System.*;
 
 public class Main {
 
     public static final String CORRECT_NUMBER = "5";
+    public static  int MAX_TRIES = 5;
+    public static int NUMBER_TRIES= 0;
 
     public static void main(String[] args) {
-        Scanner number = new Scanner(in);
-        out.println("Wpisz " + CORRECT_NUMBER + " lub inny numer");
-        String name = number.nextLine();
-        out.println(name);
-        if (CORRECT_NUMBER.equals(name)) {
-            out.println("Wszedles");
-        } else {
-            out.println("Nie prawidlowe haslo");
+        while (NUMBER_TRIES < MAX_TRIES) {
+            Scanner number = new Scanner(in);
+            out.println("Wpisz " + CORRECT_NUMBER + " lub inny numer");
+            String name = number.nextLine();
+            out.println(name);
+            if (CORRECT_NUMBER.equals(name)) {
+                out.println("Wszedles");
+                break;
+            } else {
+                NUMBER_TRIES++;
+                out.println("Nie prawidlowe haslo");
+            }
+        }
+        if (NUMBER_TRIES == MAX_TRIES)//dupa123
+        {
+            exit(0);
         }
     }
 }
