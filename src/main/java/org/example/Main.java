40dbbe61ab9f6a43733bbeb4478b2ad2a0355acb
ArtomBusniuk <151449253+ArtomBusniuk@users.noreply.github.com>
2024-05-05 19:19:25 +0100

    add menu and new function encrypt and decrypt password

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Base64;
+
 import java.util.Scanner;
 
 import static java.lang.System.*;
@@ -19,10 +21,31 @@ public class Main {
             out.println(input);
             if (CORRECT_NUMBER.equals(input)) {
                 out.println("Wszedles!");
+                menu();
                 return;
             }
             numberOfTries++;
             out.println("Nie prawidlowe haslo!\n");
         }
     }
+    public static void menu() {
+
+        Scanner number = new Scanner(in);
+        out.println("Wszedles w admina!");
+        out.println("1 - encrypt hasla  i decrypt hasla");
+        out.println("2 - exit");
+        String menuInput = number.nextLine();
+        switch (menuInput) {
+            case "1":
+                byte[] encryptPassword = Base64.getEncoder().encode(CORRECT_NUMBER.getBytes());
+                out.printf("Twoj encrypt hasla to ------> " + new String(encryptPassword));
+
+                byte[] decryptPassword = Base64.getDecoder().decode(encryptPassword);
+                out.println("\nTwoj dencrypt hasla to ------> " + new String(decryptPassword));
+                break;
+            case "2":
+                break;
+        }
+    }
+
 }
