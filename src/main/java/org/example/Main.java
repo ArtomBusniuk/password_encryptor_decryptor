b60cdae8e8a0adeb7d4483881e6c6ace5f8bf6a1
ArtomBusniuk <151449253+ArtomBusniuk@users.noreply.github.com>
2024-05-06 18:41:08 +0100

    fix after CR

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,26 +6,24 @@ import static java.lang.System.*;
 public class Main {
 
     public static final String CORRECT_NUMBER = "5";
-    public static  int MAX_TRIES = 5;
-    public static int NUMBER_TRIES= 0;
+    public static final int MAX_TRIES = 5;
+
 
     public static void main(String[] args) {
-        while (NUMBER_TRIES < MAX_TRIES) {
-            Scanner number = new Scanner(in);
-            out.println("Wpisz " + CORRECT_NUMBER + " lub inny numer");
-            String name = number.nextLine();
-            out.println(name);
-            if (CORRECT_NUMBER.equals(name)) {
-                out.println("Wszedles");
-                break;
-            } else {
-                NUMBER_TRIES++;
-                out.println("Nie prawidlowe haslo");
+        int numberTries= 0;
+        Scanner number = new Scanner(in);
+        while (numberTries < MAX_TRIES) {
+            out.printf("Wpisz %s lub inny numer:", CORRECT_NUMBER);
+            String input = number.nextLine();
+            out.println(input);
+            if (CORRECT_NUMBER.equals(input)) {
+                out.println("Wszedles!");
+                return;
             }
+                numberTries++;
+                out.println("Nie prawidlowe haslo!\n");
+
         }
-        if (NUMBER_TRIES == MAX_TRIES)//dupa123
-        {
-            exit(0);
-        }
+
     }
 }
