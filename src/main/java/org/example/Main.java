0fe3aeae4e8af1c17fc5e3591f4296c419d2b83a
ArtomBusniuk <151449253+ArtomBusniuk@users.noreply.github.com>
2024-05-09 16:09:56 +0100

    Add encrypt and decrypt given password (#8)
    * add menu and new function encrypt and decrypt password and add loop for wrong choice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import lombok.Getter;
+
+import java.util.Base64;
+
 import java.util.Scanner;
 
 import static java.lang.System.*;
@@ -9,20 +13,46 @@ public class Main {
     public static final String CORRECT_NUMBER = "5";
     public static final int MAX_TRIES = 5;
 
+    public static Scanner getScanner() {
+        return new Scanner(in);
+    }
+
 
     public static void main(String[] args) {
         int numberOfTries = 0;
-        Scanner number = new Scanner(in);
+        Scanner userScanner = getScanner();
         while (numberOfTries < MAX_TRIES) {
             out.printf("Wpisz %s lub inny numer:", CORRECT_NUMBER);
-            String input = number.nextLine();
+            String input = userScanner.nextLine();
             out.println(input);
             if (CORRECT_NUMBER.equals(input)) {
-                out.println("Wszedles!");
+                out.println("Wszedles w admina!");
+                printMenu(userScanner);
                 return;
             }
             numberOfTries++;
             out.println("Nie prawidlowe haslo!\n");
         }
     }
+
+    public static void printMenu(Scanner scanner) {
+        out.println("1 - encrypt hasla  i decrypt hasla");
+        out.println("2 - exit");
+        String menuInput = scanner.nextLine();
+        switch (menuInput) {
+            case "1":
+                byte[] encrypt = Base64.getEncoder().encode(CORRECT_NUMBER.getBytes());
+                out.printf("Twoj encrypt hasla to ------> " + new String(encrypt));
+
+                byte[] decrypt = Base64.getDecoder().decode(encrypt);
+                out.println("\nTwoj dencrypt hasla to ------> " + new String(decrypt));
+                break;
+            case "2":
+                break;
+            default:
+                out.println("Blad! Powtorz jeszcze raz");
+                printMenu(scanner);
+        }
+    }
+
 }
