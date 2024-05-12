e3dccac853339e9b42f8ad54fab696f5c4472bb5
Bartek <brtkbaranowski@gmail.com>
2024-05-12 17:20:13 +0200

    add sketch for user login

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,30 +23,51 @@ public class Main {
         int numberOfTries = 0;
         Scanner userScanner = getScanner();
         List<User> userList = new ArrayList<>();
-
-        while(numberOfTries < 5) {
-            String userDecision = "Q";
+        User admin = new User();
+        admin.username = "Admin";
+        admin.password = "qwerty";
+        userList.add(admin);
+        while (true) {
+            System.out.println("Press Q to register next user.");
+            String userDecision = userScanner.nextLine();
             if (userDecision.equals("Q")) {
                 userList.add(registerNextUser(userScanner));
                 System.out.println("Press Q to register next user.");
-
                 userDecision = userScanner.nextLine();
-                 if (userDecision.equals("Q")) {
-                     continue;
-                 }
+                if (!userDecision.equals("Q")) {
+                    break;
+                }
+            } else {
+                break;
             }
-            userList.forEach((user -> System.out.printf("User %s, password %s",user.username,user.password)));
-            if (true) {
-                //System.out.println("Hello " + inputLogin + "!");
-                printMenu(userScanner);
-                return;
-            }
+        }
+        System.out.println("Current users in your database:");
+        userList.forEach((user -> System.out.printf("User %s, password %s \n", user.username, user.password)));
 
-            ++numberOfTries;
-            System.out.println("Nie prawidlowy login albo haslo!\n");
-            System.out.println("Sprobuj jeszcze raz");
+        // check IF username IS in the userList, if there is, check if password is correct for THIS user
+        System.out.println("Podaj swoj login");
+
+        boolean userExists = findIfUserExists(userList, username);
+        if (userExists) {
+            User user = getUserFromList(username);
+            loginUser(user);
+
         }
 
+
+        ++numberOfTries;
+        System.out.println("Nie prawidlowy login albo haslo!\n");
+        System.out.println("Sprobuj jeszcze raz");
+
+    }
+
+
+    public static void loginUser(User user) {
+        //logic for checking password validity for user
+        boolean isLoggedIn = validateUserPassword(user, password);
+        if (isLoggedIn) {
+            printMenu(userScanner);
+        }
     }
 
     public static void printMenu(Scanner scanner) {
@@ -65,23 +86,25 @@ public class Main {
 //            default:
 //                System.out.println("Blad! Powtorz jeszcze raz");
 //                printMenu(scanner);
-    //}
+        //}
 
     }
-    public  static User registerUser(String username,String password) {
+
+    public static User registerUser(String username, String password) {
         User newUser = new User();
         newUser.password = password;
         newUser.username = username;
         return newUser;
 
     }
+
     public static User registerNextUser(Scanner scanner) {
         System.out.printf("Podaj login:");
-       String  inputLogin = scanner.nextLine();
+        String inputLogin = scanner.nextLine();
         System.out.printf("Podaj haslo:");
         String inputPassword = scanner.nextLine();
-        User user = registerUser(inputLogin,inputPassword);
-        System.out.printf("Hello %s, your password is %s\n",user.username,user.password);
+        User user = registerUser(inputLogin, inputPassword);
+        System.out.printf("Hello %s, your password is %s\n", user.username, user.password);
         return user;
     }
 
