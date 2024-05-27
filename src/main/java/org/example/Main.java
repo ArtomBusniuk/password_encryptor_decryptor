33f8b0aa4bebb6454cbb1ec39b8b1d07d9858a73
ArtomBusniuk <151449253+ArtomBusniuk@users.noreply.github.com>
2024-05-27 22:40:25 +0200

    WIP

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,8 @@ import java.util.Base64;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.System.in;
+
 public class Main {
     public static final int MAX_TRIES = 5;
 
@@ -15,44 +17,50 @@ public class Main {
     }
 
     public static Scanner getScanner() {
-        return new Scanner(System.in);
+        return new Scanner(in);
     }
 
     public static void main(String[] args) {
-
+        String adminLogin = "admin";
+        String adminPassword = "admin";
         int numberOfTries = 0;
         Scanner userScanner = getScanner();
         List<User> userList = new ArrayList<>();
-        User admin = new User();
-        admin.username = "Admin";
-        admin.password = "qwerty";
-        userList.add(admin);
-        while (true) {
-            System.out.println("Press Q to register next user.");
-            String userDecision = userScanner.nextLine();
+
+
+        while(numberOfTries < 5) {
+            System.out.println("Press Q to register user or press L to login.");
+            String userDecision =  userScanner.nextLine();
+            if (userDecision.equals("L")) {
+                System.out.println("Podaj login:");
+                String loginInput = userScanner.nextLine();
+                System.out.println("Podaj haslo:");
+                String passwordInput = userScanner.nextLine();
+                for ( int i = 0;  i < userList.size(); i++)
+                    if (userList.get(i).username.equals(loginInput) && userList.get(i).password.equals(passwordInput)) {
+                        printMenu(userScanner);
+                    }
+                    else {
+                        ++numberOfTries;
+                        System.out.println("Nie prawidlowy login albo haslo!\n");
+                        System.out.println("Sprobuj jeszcze raz");
+                    }
+
+            }
             if (userDecision.equals("Q")) {
                 userList.add(registerNextUser(userScanner));
-                System.out.println("Press Q to register next user.");
                 userDecision = userScanner.nextLine();
-                if (!userDecision.equals("Q")) {
-                    break;
-                }
-            } else {
-                break;
+                 if (userDecision.equals("Q")) {
+                     continue;
+                 }
+                //userList.forEach((user -> System.out.printf("User %s, password %s",user.username,user.password)));
             }
+
+
+
+
+
         }
-        System.out.println("Current users in your database:");
-        userList.forEach((user -> System.out.printf("User %s, password %s \n", user.username, user.password)));
-
-        // check IF username IS in the userList, if there is, check if password is correct for THIS user
-        System.out.println("Podaj swoj login");
-
-
-
-
-        ++numberOfTries;
-        System.out.println("Nie prawidlowy login albo haslo!\n");
-        System.out.println("Sprobuj jeszcze raz");
 
     }
 
@@ -62,37 +70,45 @@ public class Main {
         System.out.println("2 - exit");
         switch (scanner.nextLine()) {
             case "1":
-                byte[] encrypt = Base64.getEncoder().encode(registerUser(user).getBytes());
-                System.out.printf("Twoj encrypt hasla to ------> " + new String(encrypt));
-                byte[] decrypt = Base64.getDecoder().decode(encrypt);
-                System.out.println("\nTwoj dencrypt hasla to ------> " + decrypt);
-            case "2":
+                byte[] encrypt = Base64.getEncoder().encode(registerUser("Login","Login").password.getBytes());
+               System.out.printf("Twoj encrypt hasla to ------> " + new String(encrypt));
+               byte[] decrypt = Base64.getDecoder().decode(encrypt);
+               System.out.println("\nTwoj dencrypt hasla to ------> " + decrypt);
+               case "2":
                 break;
-            default:
+           default:
                 System.out.println("Blad! Powtorz jeszcze raz");
                 printMenu(scanner);
         }
 
     }
-
-    public static User registerUser(String username, String password) {
+    public  static User registerUser(String username,String password) {
         User newUser = new User();
         newUser.password = password;
         newUser.username = username;
         return newUser;
 
     }
-
+//    public static User adminUser(String adminLogin,String adminPassword){
+//        User admin = new User();
+//        admin.adminLogin = adminLogin;
+//        admin.adminPassword = adminPassword;
+//        return  admin;
+//    }
     public static User registerNextUser(Scanner scanner) {
         System.out.printf("Podaj login:");
-        String inputLogin = scanner.nextLine();
+        String  inputLogin = scanner.nextLine();
         System.out.printf("Podaj haslo:");
         String inputPassword = scanner.nextLine();
-        User user = registerUser(inputLogin, inputPassword);
-        System.out.printf("Hello %s, your password is %s\n", user.username, user.password);
+        User user = registerUser(inputLogin,inputPassword);
+        System.out.printf("Hello %s, your password is %s\n",user.username,user.password);
         return user;
     }
-
+    public static User loginUser(boolean loggeIn) {
+        User login = new User();
+        login.loggedIn = loggeIn;
+        return login;
+    }
 
 
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class User {
+     public String adminLogin = "admin";
+     public String adminPassword = "admin";
      public  String username;
      public   String password;
      public static String type = "USER";
